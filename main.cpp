75b067df33c3239a5d416ce78c61ac4f2eed8779
DESKTOP-LDRG8J8\Tapok <Tapok134224@mail.ru>
2023-08-07 00:29:19 +0300

    first green triangle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #include "SDL2/SDL.h"
 #include <GL/glew.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 #undef main
 
@@ -9,9 +12,73 @@
 float vertices[] = {
     -0.5f, -0.5f, 0.0f, //
     0.5f,  -0.5f, 0.0f, //
-    0.0f,  0.5f,  0.0f,
+    0.0f,  0.5f,  0.0f  //
 };
 
+struct ShaderSources {
+    std::string vertexSource;
+    std::string fragmentSource;
+};
+
+static ShaderSources parseShader(const std::string& path) {
+    std::ifstream stream(path);
+
+    enum class ShaderType { NONE = -1, VERTEX = 0, FRAGMENT = 1 };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    while (std::getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            } else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        } else
+            ss[(int)type] << line << '\n';
+    }
+    return {ss[0].str(), ss[1].str()};
+}
+
+static unsigned int compileShader(const std::string& source, unsigned int shaderType) {
+    uint32_t id = glCreateShader(shaderType);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int lenght;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+        char* message = (char*)alloca(lenght * sizeof(char));
+        glGetShaderInfoLog(id, lenght, &lenght, message);
+        std::cout << "Failed to compile shader"
+                  << "\n";
+        std::cout << message << "\n";
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static uint32_t createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    uint32_t program = glCreateProgram();
+    uint32_t vs = compileShader(vertexShader, GL_VERTEX_SHADER);
+    uint32_t fs = compileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
 
 SDL_Window* initWindow() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -43,7 +110,8 @@ void* initOpenGl(SDL_Window* window) {
     }
     return context;
 }
-void closeWindow(SDL_Window* window, void* context) {
+void closeWindow(SDL_Window* window, void* context, uint32_t shader) {
+    glDeleteProgram(shader);
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
     glUseProgram(0);
@@ -58,11 +126,18 @@ int main() {
     auto context = initOpenGl(window);
     if (!context) return 1;
 
-    uint32_t vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    uint32_t VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
+
+    ShaderSources sources = parseShader("../res/shaders/base.glsl");
+    uint32_t shader = createShader(sources.vertexSource, sources.fragmentSource);
+    glUseProgram(shader);
 
     bool should_run = true;
     while (should_run) {
@@ -72,20 +147,21 @@ int main() {
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glDrawArrays(GL_TRIANGLES,0,3);
-//            glDrawElements(GL_TRIANGLES, 3,)
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //            glDrawElements(GL_TRIANGLES, 3,)
 
             SDL_GL_SwapWindow(window);
 
             switch (event.type) {
             case SDL_QUIT:
-                closeWindow(window, context);
+                closeWindow(window, context, shader);
                 should_run = false;
                 break;
             case SDL_KEYDOWN:
                 auto keys = SDL_GetKeyboardState(nullptr);
                 if (keys[SDL_SCANCODE_ESCAPE]) {
-                    closeWindow(window, context);
+                    closeWindow(window, context, shader);
                     should_run = false;
                 }
                 break;
@@ -95,21 +171,3 @@ int main() {
 
     return 0;
 }
-
-// int Cleanup() {
-//     glUseProgram(0);
-//     glDisableVertexAttribArray(0);
-//     glDetachShader(m_shader_prog, m_vert_shader);
-//     glDetachShader(m_shader_prog, m_frag_shader);
-//     glDeleteProgram(m_shader_prog);
-//     glDeleteShader(m_vert_shader);
-//     glDeleteShader(m_frag_shader);
-//     glDeleteTextures(1, &m_tex);
-//     glDeleteBuffers(1, &m_ebo);
-//     glDeleteBuffers(1, &m_vbo);
-//     glDeleteVertexArrays(1, &m_vao);
-//     SDL_GL_DeleteContext(m_context);
-//     SDL_DestroyWindow(m_window);
-//     SDL_Quit();
-//     return 0;
-// }
